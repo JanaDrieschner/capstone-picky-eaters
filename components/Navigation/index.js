309c5c49acd245a2b276a61fa5bd4fb186f5156e
Jana Drieschner-Aronova <j.drieschner@hotmail.com>
2023-03-24 17:00:21 +0100

    add style

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -1,19 +1,53 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import styled from "styled-components";
+import { FaHome } from "react-icons/fa";
+import { GrFavorite } from "react-icons/gr";
+import Image from "next/image";
 
 export default function Navigation({ children }) {
+  const router = useRouter();
   return (
-    <section>
-      <ul>
+    <StyledFooter>
+      <StyledList>
         <li>
-          <Link href="/">Home</Link>
+          <StyledLink href="/">
+            <FaHome />
+          </StyledLink>
         </li>
         <li>
-          <Link href="/favorites">Favorite Recipes</Link>
+          <StyledLink href="/favorites">
+            {" "}
+            <GrFavorite />
+          </StyledLink>
         </li>
-      </ul>
-    </section>
+      </StyledList>
+    </StyledFooter>
   );
 }
+
+const StyledFooter = styled.footer`
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 80px;
+  background-color: white;
+  color: white;
+  text-align: center;
+  border: 3px solid black;
+`;
+
+const StyledList = styled.ul`
+  list-style-type: none;
+  display: flex;
+  justify-content: space-around;
+`;
+
+const StyledLink = styled(Link)`
+  font-size: 30px;
+  pading: 10px 25px;
+`;
 
 /*togglelogik
 svgs*/
