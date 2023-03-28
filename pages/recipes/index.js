cc029a444c296358a51dbdc4af70c9f2db2aaf43
Jana Drieschner-Aronova <j.drieschner@hotmail.com>
2023-03-28 16:44:20 +0200

    add recipes POST

--- pages/recipes/index.js
+++ pages/recipes/index.js
@@ -1,5 +1,26 @@
 import RecipeList from "../../components/RecipeList";
 
 export default function Recipes({ id }) {
+  async function handleCreateRecipe(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const recipeData = Object.fromEntries(formData);
+
+    const response = await fetch("api/recipes", {
+      method: "POST",
+      body: JSON.stringify(recipeData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      await response.json();
+    } else {
+      console.error(`Error: ${response.status}`);
+    }
+  }
+
   return <RecipeList recipe={id} />;
 }
