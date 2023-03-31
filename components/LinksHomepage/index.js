963bc0dd4a07ead6e8a96ddd48a9c9e75fc932aa
Jana Drieschner-Aronova <j.drieschner@hotmail.com>
2023-03-31 08:50:22 +0200

    add routing

--- components/LinksHomepage/index.js
+++ components/LinksHomepage/index.js
@@ -2,26 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import Link from "next/link";
 import { HiLink } from "react-icons/hi";
-import CreateOwnRecipe from "../OwnRecipeForm";
+import { useRouter } from "next/router";
+
 import { useState } from "react";
-import Modal from "../Modal";
 
 export default function LinksHomepage() {
-  /*const [showModal, setShowModal] = useState(false);
-
-  const handleOpenModal = () => {
-    setShowModal(true);
-  };
-  const handleCloseModal = () => {
-    setShowModal(false);
-  };
-
-  /*<StyledNextLink onClick={handleAddRecipe}>
-  Add my Recipes <HiLink />
-</StyledNextLink>*/
-
   const [link, setLink] = useState("");
   const [title, setTitle] = useState("");
+  const router = useRouter();
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -34,29 +22,34 @@ export default function LinksHomepage() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    onClose();
+    router.push("/links");
   };
 
   return (
     <>
       <StyledWrapper>
         <StyledForm onSubmit={handleSubmit}>
+          <StyledTitle>
+            <p>My Recipes</p>
+          </StyledTitle>
           <label htmlFor="link">Link</label>
+          <br />
           <input
             type="text"
             name="link"
             value={link}
             onChange={handleChange}
           />{" "}
+          <StyledButton type="submit">Safe</StyledButton>
           <br /> <br />
           <label htmlFor="title">Title</label>
+          <br />
           <input
             type="text"
             name="title"
             value={title}
             onChange={handleChange}
           />
-          <button type="submit">Safe</button>
         </StyledForm>
 
         <StyledLink href="/recipes/">Inspire Me</StyledLink>
@@ -71,29 +64,7 @@ const StyledWrapper = styled.div`
   align-items: center;
   height: 100vh;
   flex-direction: column;
-`;
-
-const StyledLink = styled(Link)`
-
-text-decoration: none;
-  padding 1.3em 3em;
-  white-space: nowrap;
-  margin: 10px;
-  text-transform: uppercase;
-  font-weight: 500;
-  
-  color: #F4F5F6;
-  background-color:  #0F5C64;     
-  border: 1px ridge #BDC0BF;
-  border-radius: 45px;
-  box-shadow: 0px 8px 15px (rgba 0, 0, 0, 0.1);
-  transition: all 0.3s ease 0s;
-  &:hover {
-    background-color: #0f5c64;
-    box-shadow: 0px 15 px 20px FaRegIdBadge(13, 240, 252, 0.4);
-    color: #86887b;
-    transform: translateY(-7px);
-  }
+  margin-top: -25px;
 `;
 
 const StyledForm = styled.form`
@@ -101,6 +72,45 @@ const StyledForm = styled.form`
 text-decoration: none;
   padding 1.3em 3em;
   white-space: nowrap;
+  margin: 7px;
+  text-transform: uppercase;
+  font-weight: 500;
+  
+  color: #F4F5F6;
+  background-color:  #0F5C64;     
+  border: 1px ridge #BDC0BF;
+  border-radius: 45px;
+  box-shadow: 0px 8px 15px (rgba 0, 0, 0, 0.1);
+  transition: all 0.3s ease 0s;
+  &:hover {
+    background-color: #0f5c64;
+    box-shadow: 0px 15 px 20px FaRegIdBadge(13, 240, 252, 0.4);
+    color: #86887b;
+    transform: translateY(-7px);
+  }
+
+
+`;
+const StyledButton = styled.button`
+  border-radius: 20px;
+  background-color: #f4f5f5;
+  color: #0f5c64;
+  border: 1px solid #bdc0bf;
+  border-radius: 45px;
+  box-shadow: 0px 8px 15px (rgba 0, 0, 0, 0.1);
+  width: 30%;
+`;
+const StyledTitle = styled.p`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: -10px;
+`;
+
+const StyledLink = styled(Link)`
+text-decoration: none;
+  padding 1.3em 3em;
+  white-space: nowrap;
   margin: 10px;
   text-transform: uppercase;
   font-weight: 500;
@@ -117,4 +127,6 @@ text-decoration: none;
     color: #86887b;
     transform: translateY(-7px);
   }
-  `;
+
+
+`;
