61be1580cb83b2dd7e5c158e76f105edb12a038a
Jana Drieschner-Aronova <j.drieschner@hotmail.com>
2023-04-06 17:18:19 +0200

    add localstorage

--- components/OwnDetailPage/index.js
+++ components/OwnDetailPage/index.js
@@ -1,11 +1,10 @@
 import styled from "styled-components";
 import ReactPlayer from "react-player";
 import Heading from "../Heading";
-
-import { useState } from "react";
+import useLocalStorageState from "use-local-storage-state";
 
 export default function OwnRecipeDetails() {
-  const [note, setNote] = useState("");
+  const [note, setNote, isNoteSaved] = useLocalStorageState("note", "");
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -14,6 +13,16 @@ export default function OwnRecipeDetails() {
   const handleChange = (event) => {
     setNote(event.target.value);
   };
+
+  const handleSaveNote = () => {
+    setNoteSaved(true);
+  };
+
+  const handleDeleteNote = () => {
+    setNote("");
+    setNoteSaved(false);
+  };
+
   return (
     <>
       <Heading>My Recipe</Heading>
@@ -26,25 +35,36 @@ export default function OwnRecipeDetails() {
       <form onSubmit={handleSubmit}>
         <StyledTextarea value={note} name="notes" onChange={handleChange} />
         <br /> <br />
-        <StyledButton type="submit">safe</StyledButton>
+        <StyledSection>
+          <StyledButton
+            type="submit"
+            onClick={handleSaveNote}
+            disabled={isNoteSaved}
+          >
+            {isNoteSaved ? "Saved" : "Save"}
+          </StyledButton>
+          <StyledButton onClick={handleDeleteNote}>Delete</StyledButton>
+        </StyledSection>
       </form>
     </>
   );
 }
-
+//margin: 2rem 0 2rem;
 const StyledPlayer = styled.div`
   width: 100%;
   height: 30vh;
   display: flex;
   justify-content: center;
   align-items: center;
-  margin: 2rem 0 2rem;
+  margin-top: 2rem;
+  border: 3px solid black;
 `;
 
 const StyledTextarea = styled.textarea`
   width: 100%;
   height: 30vh;
   margin-top: 5rem;
+
   font-family: nunito, sans-serif;
   display: block;
   padding: 1rem;
@@ -52,6 +72,11 @@ const StyledTextarea = styled.textarea`
   resize: none;
   border: 5px solid #0f5c64;
   border-radius: 20px;
+`;
+
+const StyledSection = styled.section`
+  display: flex;
+  justify-content: space-around;
 `;
 
 const StyledButton = styled.button`
